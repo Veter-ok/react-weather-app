190fd593dc6699daddf54761fd58b95aa9ad2aa4
Lavrov Rodion <61391385+Veter-ok@users.noreply.github.com>
2023-03-09 20:16:21 +0300

    Add snow falling

---
+++ src/components/UI/showFall/snowFall.tsx
@@ -0,0 +1,32 @@
+import React, {FunctionComponent as FC} from 'react'
+import './snowFall.css'
+
+interface Props {
+	snowFall: number
+}
+
+const SnowFall:FC<Props> = ({snowFall}) => {
+
+	const createSnow = () => {
+		let snowList = []
+		for(let i = 0; i < 50; i++){
+			snowList.push(<div className='snow' style={{
+				marginLeft: `${Math.random() * (0 - 1000) + 1000}px`,
+				animationDelay: `${Math.random() * (2 - 7) + 7}s`
+			}}></div>)
+		}
+		return snowList
+	}
+
+	return (
+	<div>
+		{snowFall > 0 ?
+			<>{createSnow()}</>
+		:
+			<></>
+		}
+	</div>
+	)
+}
+
+export default SnowFall
--- src/components/screens/RainyScreen/PictureScreen.tsx
+++ src/components/screens/RainyScreen/PictureScreen.tsx
@@ -6,6 +6,7 @@ import Clouds from "../../UI/clouds/clouds";
 import Hill from "../../drawing/hills/hill";
 import { WeatherDataContext } from "../../../context/WeatherDataProvider";
 import { WeatherOWAPIDataContext } from "../../../context/WeatherDataProviderOWAPI";
+import SnowFall from "../../UI/showFall/snowFall";
 
 export const PictureThemeContext = createContext<string>("")
 
@@ -20,7 +21,7 @@ const PictureScreen:FC = () => {
 		const time = date.getHours()
 		let createTheme = ''
 
-		if (currentlyWeather.snowDepth > 0){
+		if (currentlyWeather.snowDepth > 0.05){
 			createTheme += 'winter-'
 		}else{
 			createTheme += 'summer-'
@@ -45,6 +46,7 @@ const PictureScreen:FC = () => {
 			<div className={`frame ${theme}`}>
 				<Luminary cloudcover={currentlyWeather.cloudcover}/>
 				<Rainfall rain={currentlyWeather.rain}/>
+				<SnowFall snowFall={currentlyWeather.snowfall}/>
 				<Clouds/>
 				<Hill/>
 			</div>
