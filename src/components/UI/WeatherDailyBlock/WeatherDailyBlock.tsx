eb1eea240dcc8ad3b3a31bdee16807188c1b4abf
Lavrov Rodion <61391385+Veter-ok@users.noreply.github.com>
2023-03-17 22:21:50 +0300

    Update daily and hourly weather

--- src/components/UI/WeatherDailyBlock/WeatherDailyBlock.tsx
+++ src/components/UI/WeatherDailyBlock/WeatherDailyBlock.tsx
@@ -2,13 +2,11 @@ import {FunctionComponent as FC, useContext} from "react";
 import './WeatherDailyBlock.css'
 import { DarkModeContext } from "../../../context/DarkModeProvider";
 import { WeatherDataContext} from "../../../context/WeatherDataProvider";
-import { convertStringToDate } from "../../../utils/FormatDate";
 
 export const WeatherDailyBlock:FC = () => {
-	const {hourlyWeather, dailyWeather} = useContext(WeatherDataContext)
+	const {dailyWeather} = useContext(WeatherDataContext)
 	const darkMode = useContext(DarkModeContext)
 
-	const convertTime = (date: Date):string => `${date.getHours()}:00`
 	console.log(dailyWeather)
 
 	return (
@@ -17,7 +15,7 @@ export const WeatherDailyBlock:FC = () => {
 				<div key={index} className={darkMode ? "daily-block block-dark" : "daily-block block-light"}>
 					<div className="daily-block-1">
 						<div className="daily-block-date">{dailyWeather.times[index].split("-")[2]}.{dailyWeather.times[index].split("-")[1]}</div>
-						<div className="daily-block-temperature">{hourlyWeather.temperatures[index]}°C</div>
+						<div className="daily-block-temperature">from {Math.round(dailyWeather.temperatures_min[index])} to {Math.round(dailyWeather.temperatures_max[index])}°C</div>
 					</div>
 					<div className="daily-block-2">
 						<div className="wind">Wind: E {dailyWeather.windspeed[index]}km/h</div>
--- src/components/UI/WeatherHourlyBlock/WeatherHourlyBlock.tsx
+++ src/components/UI/WeatherHourlyBlock/WeatherHourlyBlock.tsx
@@ -17,7 +17,7 @@ export const WeatherHourlyBlock:FC = () => {
 				<div key={index} className={darkMode ? "hourly-block block-dark" : "hourly-block block-light"}>
 					<div className="hourly-block-1">
 						<div className="hourly-block-date">{convertTime(convertStringToDate(hourlyWeather.times[firstIndex + index]))}</div>
-						<div className="hourly-block-temperature">{hourlyWeather.temperatures[firstIndex + index]}°C</div>
+						<div className="hourly-block-temperature">{Math.round(hourlyWeather.temperatures[firstIndex + index])}°C</div>
 					</div>
 					<div className="hourly-block-2">
 						<div className="wind">Wind: E {hourlyWeather.windSpeed[firstIndex + index]}km/h</div>
