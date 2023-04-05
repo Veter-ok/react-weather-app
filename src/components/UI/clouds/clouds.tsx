c153945dce904363c35dbeb43a3c434f2fc9deb3
Lavrov Rodion <61391385+Veter-ok@users.noreply.github.com>
2023-04-05 22:14:19 +0300

    Fix bugs

--- src/components/UI/clouds/clouds.tsx
+++ src/components/UI/clouds/clouds.tsx
@@ -2,26 +2,14 @@ import React, {FunctionComponent as FC} from 'react'
 import Cloud from '../../drawing/cloud/cloud'
 
 const Clouds:FC = () => {
-	
-	const getClouds = () => {
-		let cloudsList = []
-		for(let i = 0; i < 7; i++){
-			let randomX = Math.random() * (0 - 1000) + 1000
-			let randomY = Math.random() * (0 - 150) + 150
-			cloudsList.push(<div key={i} style={{position: 'absolute', left:`${randomX}px`, top:`${randomY}px`}}><Cloud/></div>)
-		}
-		return cloudsList
-	}
-
 	return (
 		<>
-			{/* {getClouds()} */}
-			<div className="clouds" style={{position: 'absolute', width:"1000px", height:"400px"}}>
-				<div style={{position: 'absolute', left:"19%", top:"5%"}}><Cloud/></div>
-				<div style={{position: 'absolute', left:"30%", top:"30%"}}><Cloud/></div>
-				<div style={{position: 'absolute', left:"45%", top:"22%"}}><Cloud/></div>
-				<div style={{position: 'absolute', left:"62%", top:"35%"}}><Cloud/></div>
-				<div style={{position: 'absolute', left:"78%", top:"15%"}}><Cloud/></div>
+			<div className="clouds">
+				<Cloud left="530px" top="80px"/>
+				<Cloud left="20px" top="150px"/>
+				<Cloud left="300px" top="115px"/>
+				<Cloud left="720px" top="140px"/>
+				<Cloud left="900px" top="60px"/>
 			</div>
 		</>
 	)
--- src/components/drawing/cloud/cloud.tsx
+++ src/components/drawing/cloud/cloud.tsx
@@ -1,10 +1,15 @@
 import React, {FunctionComponent as FC} from 'react'
 import './cloud.css'
 
-const Cloud:FC = () => {
+interface IPropsCloud {
+	left: string,
+	top: string,
+}
+
+const Cloud:FC<IPropsCloud> = ({left, top}) => {
 
 	return (
-		<div className="cloud">
+		<div className="cloud" style={{left: left, top: top}}>
 			<div className="cloud-circle cloud-circle-1"></div>
 			<div className="cloud-circle cloud-circle-2"></div>
 			<div className="cloud-circle cloud-circle-3"></div>
