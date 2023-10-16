2acf8d00ad9782b4520966f2ee5cbaf761c06dc7
Lavrov Rodion <61391385+Veter-ok@users.noreply.github.com>
2023-10-16 20:39:31 +0300

    Delete needless variable

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,2 @@
-export const GEO_OPTIONS = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY ? process.env.REACT_APP_RAPIDAPI_KEY : '',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
-
 export const OPEN_METEO_API_URL = 'https://api.open-meteo.com/v1/forecast?'
-
 export const OPEN_WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/'
