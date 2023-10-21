1b9d43e5cab860090e638f8a874378f8d6ded31d
Lavrov Rodion <61391385+Veter-ok@users.noreply.github.com>
2023-10-21 16:39:43 +0300

    refact hill

--- src/components/drawing/hills/hill.tsx
+++ src/components/drawing/hills/hill.tsx
@@ -1,18 +1,20 @@
-import React, {FunctionComponent as FC, useContext} from 'react'
+import {FunctionComponent as FC, useContext} from 'react'
 import './hill.css'
 import { PictureThemeContext } from '../../screens/PictureScreen'
+import { colors_1, colors_2 } from './colors'
 
 const Hill:FC = () => {
 	const pictureTheme = useContext(PictureThemeContext)
-
+	const type = `${pictureTheme.season}-${pictureTheme.timeOfDay}-${pictureTheme.cloudCover}`
+	
 	return (
 		<>
-			<div className={`hill hill-1 hill-${pictureTheme.season}-${pictureTheme.cloudCover === "clear" ? '' : `${pictureTheme.cloudCover}-`}${pictureTheme.timeOfDay}-1`}></div>
-			<div className={`hill hill-2 hill-${pictureTheme.season}-${pictureTheme.cloudCover === "clear" ? '' : `${pictureTheme.cloudCover}-`}${pictureTheme.timeOfDay}-1`}></div>
-			<div className={`hill hill-3 hill-${pictureTheme.season}-${pictureTheme.cloudCover === "clear" ? '' : `${pictureTheme.cloudCover}-`}${pictureTheme.timeOfDay}-1`}></div>
-			<div className={`hill hill-4 hill-${pictureTheme.season}-${pictureTheme.cloudCover === "clear" ? '' : `${pictureTheme.cloudCover}-`}${pictureTheme.timeOfDay}-2`}></div>
-			<div className={`hill hill-5 hill-${pictureTheme.season}-${pictureTheme.cloudCover === "clear" ? '' : `${pictureTheme.cloudCover}-`}${pictureTheme.timeOfDay}-2`}></div>
-			<div className={`hill hill-6 hill-${pictureTheme.season}-${pictureTheme.cloudCover === "clear" ? '' : `${pictureTheme.cloudCover}-`}${pictureTheme.timeOfDay}-2`}></div>	
+			<div className='hill hill-1' style={{backgroundColor: colors_1[type]}}></div>
+			<div className='hill hill-2' style={{backgroundColor: colors_1[type]}}></div>
+			<div className='hill hill-3' style={{backgroundColor: colors_1[type]}}></div>
+			<div className='hill hill-4' style={{backgroundColor: colors_2[type]}}></div>
+			<div className='hill hill-5' style={{backgroundColor: colors_2[type]}}></div>
+			<div className='hill hill-6' style={{backgroundColor: colors_2[type]}}></div>	
 		</>
 	)
 }
