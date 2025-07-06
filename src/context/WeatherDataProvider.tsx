4199da06b98743ff1f7411d1f15e01cd3addf5a7
Rodion Lavrov <lrd.msk@gmail.com>
2025-07-06 15:00:26 +0300

    refact(WeatherDataProvider): separate states in WeatherDataProvider

--- src/context/WeatherDataProvider.tsx
+++ src/context/WeatherDataProvider.tsx
@@ -1,5 +1,5 @@
-import React, {createContext, FunctionComponent as FC, useEffect, useState} from "react"; 
-import { IDailyWeather, IHourlyWeatherData, IweatherData } from "../types/weatherDataType";
+import {createContext, FunctionComponent as FC, useEffect, useState} from "react"; 
+import { ICurrentlyWeatherData, IDailyWeather, IHourlyWeatherData, IweatherData } from "../types/weatherDataType";
 import { OPEN_METEO_API_URL } from "../api/api";
 import { ResponseWeather } from "./types";
 
@@ -11,50 +11,54 @@ interface IWeatherDataProviderProps {
 interface IFullWeatherData extends IweatherData {
 	hourlyWeather: IHourlyWeatherData
 	dailyWeather: IDailyWeather
+	currentlyWeather: ICurrentlyWeatherData
+}
+
+const defaultCurrentlyWeather:ICurrentlyWeatherData = {
+	time: new Date(),
+	timezone: "Europe/Moscow",
+	sunrise: new Date(),
+	sunset: new Date(),
+	weather: '',
+	temperature: 0,
+	humidity: 0,
+	windSpeed: 0,
+	cloudcover: 0,
+	rain: 0,
+	snowfall: 0,
+	snowDepth: 0
+}
+
+const defaultHourlyWeather:IHourlyWeatherData = {
+	times: [],
+	temperatures: [],
+	humidity: [],
+	windSpeed: [],
+	cloudcover: [],
+	rain: [],
+	snowfall: [],
+	snow_depth: []
+}
+
+const defaultDailyWeather:IDailyWeather = {
+	times: [],
+	temperatures_max: [],
+	temperatures_min: [],
+	windspeed: [],
+	rain_sum: [],
+	snowfall_sum: [],
 }
 
 const WeatherDataContext = createContext<IFullWeatherData>({
-	hourlyWeather: {
-		times: [],
-		temperatures: [],
-		humidity: [],
-		windSpeed: [],
-		cloudcover: [],
-		rain: [],
-		snowfall: [],
-		snow_depth: []
-	},
-	dailyWeather: {
-		times: [],
-		temperatures_max: [],
-		temperatures_min: [],
-		windspeed: [],
-		rain_sum: [],
-		snowfall_sum: [],
-	}
+	hourlyWeather: defaultHourlyWeather,
+	dailyWeather: defaultDailyWeather,
+	currentlyWeather: defaultCurrentlyWeather
 })
 
 const WeatherDataProvider:FC<IWeatherDataProviderProps> = ({coordinates, children}) => {
-	const [weatherData, setWeatherData] = useState<IFullWeatherData>({
-		hourlyWeather: {
-			times: [],
-			temperatures: [],
-			humidity: [],
-			windSpeed: [],
-			cloudcover: [],
-			rain: [],
-			snowfall: [],
-			snow_depth: [],
-		},
-		dailyWeather: {
-			times: [],
-			temperatures_max: [],
-			temperatures_min: [],
-			windspeed: [],
-			rain_sum: [],
-			snowfall_sum: [],
-		}
-	})
+	const [currentlyWeather, setCurrentlyWeather] = useState<ICurrentlyWeatherData>(defaultCurrentlyWeather)
+	const [hourlyWeather, setHourlyWeather] = useState<IHourlyWeatherData>(defaultHourlyWeather)
+	const [dailyWeather, setDailyWeather] = useState<IDailyWeather>(defaultDailyWeather)
 
 	const convertStringsToDate = (data: string[]) => {
 		const new_array:Date[] = []
@@ -65,29 +69,28 @@ const WeatherDataProvider:FC<IWeatherDataProviderProps> = ({coordinates, childre
 	}
 
 	useEffect(() => {
-		fetch(`${OPEN_METEO_API_URL}latitude=${coordinates.lat}&longitude=${coordinates.lon}&hourly=temperature_2m,relativehumidity_2m,cloudcover,windspeed_10m,snowfall,rain,snow_depth&daily=temperature_2m_max,temperature_2m_min,rain_sum,snowfall_sum,windspeed_10m_max&timezone=auto`)
+		fetch(`${OPEN_METEO_API_URL}latitude=${coordinates.lat}&longitude=${coordinates.lon}&current=temperature_2m&hourly=temperature_2m,relativehumidity_2m,cloudcover,windspeed_10m,snowfall,rain,snow_depth&daily=temperature_2m_max,temperature_2m_min,rain_sum,snowfall_sum,windspeed_10m_max&timezone=auto`)
 		.then((response) => response.json())
 		.then((data: ResponseWeather) => {
-			setWeatherData({
-				hourlyWeather: {
-					times: convertStringsToDate(data.hourly.time), 
-					temperatures: data.hourly.temperature_2m,
-					humidity: data.hourly.relativehumidity_2m,
-					windSpeed: data.hourly.windspeed_10m,
-					cloudcover: data.hourly.cloudcover,
-					rain: data.hourly.rain,
-					snowfall: data.hourly.snowfall,
-					snow_depth: data.hourly.snow_depth
-				},
-				dailyWeather: {
-					times: convertStringsToDate(data.daily.time),
-					temperatures_max: data.daily.temperature_2m_max,
-					temperatures_min: data.daily.temperature_2m_min,
-					windspeed: data.daily.windspeed_10m_max,
-					rain_sum: data.daily.rain_sum,
-					snowfall_sum: data.daily.snowfall_sum,
-				}		
+			setHourlyWeather({
+				times: convertStringsToDate(data.hourly.time), 
+				temperatures: data.hourly.temperature_2m,
+				humidity: data.hourly.relativehumidity_2m,
+				windSpeed: data.hourly.windspeed_10m,
+				cloudcover: data.hourly.cloudcover,
+				rain: data.hourly.rain,
+				snowfall: data.hourly.snowfall,
+				snow_depth: data.hourly.snow_depth
 			})
+			setDailyWeather({
+				times: convertStringsToDate(data.daily.time),
+				temperatures_max: data.daily.temperature_2m_max,
+				temperatures_min: data.daily.temperature_2m_min,
+				windspeed: data.daily.windspeed_10m_max,
+				rain_sum: data.daily.rain_sum,
+				snowfall_sum: data.daily.snowfall_sum,
+			})
+			setCurrentlyWeather(defaultCurrentlyWeather)
 		})
 		.catch((error) => {
 			console.log(error)
@@ -95,7 +98,7 @@ const WeatherDataProvider:FC<IWeatherDataProviderProps> = ({coordinates, childre
 	}, [coordinates.lat, coordinates.lon])
 
 	return (
-		<WeatherDataContext.Provider value={weatherData}>
+		<WeatherDataContext.Provider value={{currentlyWeather, hourlyWeather, dailyWeather}}>
 			{children}
 		</WeatherDataContext.Provider>
 	)
